ef0349d7af7601aa1dced9a48b4c54aa37e3ea43
gh-joao-carvalho <joao.carvalho@greyhoundsystems.pt>
2026-01-05 18:01:11 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true }, // importante em export estático
+  trailingSlash: true, // ajuda com rotas em hospedagem estática
 };
 
 export default nextConfig;

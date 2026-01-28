cef7124d41ab8bb1f63d5ddadf49ba80bcb8e2d9
gh-joao-carvalho <joao.carvalho@greyhoundsystems.pt>
2026-01-28 14:42:25 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: true, 
 };
 
 export default nextConfig;
